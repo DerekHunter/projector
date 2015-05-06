fd74b68943822c92ad8e22ba6cccb35d8e468488
Derek Hunter <Derek@ifit.com>
2015-05-06 16:50:59 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+angular.module('hoursTracker', [])
+	.controller('projectController', function(){
+		var projectList = this;
+		projectList.projects = [
+			{name:'iosRunning', done:true},
+			{name:'narwhal', done:false}];
+
+		projectList.addProject = function() {
+			projectList.projects.push({name:projectList.projectName, done:false});
+			projectList.projectName = ' ';
+		};
+		
+	});
